ecd14d14686e45c2b66f60083e1b55c7427e9ca6
SaiMohanSattari-AidenAI <saimohan.sattari03@aidenai.com>
2024-10-25 20:30:23 +0530

    styled components installed

--- vaishnaviConstructions/src/App.jsx
+++ vaishnaviConstructions/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import AboutePage from './pages/aboutPage/AboutPage'
 import HomePage from './pages/homePage/HomePage'
-
 
 
 function App() {
@@ -12,6 +12,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<HomePage />} />
+          <Route path='/about' element={<AboutePage />} />
         </Routes>
       </BrowserRouter>
     </>
